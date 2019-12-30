ebe11bd2ab0df82191c78db1101a9d9558ea81d7
Yash Gupta <yg123yash@gmail.com>
2019-12-30 17:09:07 +0530

    Added Total Expenses And Count

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
 import ExpenseListItem from "./ExpenseListItem";
-import ExpenseSummary from "./ExpensesSummary";
 import selectExpenses from "../selectors/expenses";
 
 const ExpenseList = props => (
   <div>
     <h1>{props.expenses.length < 1 ? "No Expenses" : "Expense List"}</h1>
-    <ExpenseSummary />
+
     {props.expenses.map(expense => {
       return <ExpenseListItem key={expense.id} {...expense} />;
     })}
